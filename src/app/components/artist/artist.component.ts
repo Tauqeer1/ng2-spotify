ca7d1051e51e1de46e295330c19bb7458d266315
Tauqeer1 <tauqeer.shakir@yahoo.com>
2017-05-04 01:46:21 +0500

    Add: Getting artist from api

--- src/app/components/artist/artist.component.ts
+++ src/app/components/artist/artist.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SpotifyService } from './../../services/spotify.service';
 
+import { Artist } from './../../models/artist.model';
+import { Album } from './../../models/album.model';
 
 @Component({
     selector: 'artist',
@@ -7,7 +11,26 @@ import { Component } from '@angular/core';
     styleUrls: ['./artist.component.css']
 })
 
-export class ArtistComponent {
+export class ArtistComponent implements OnInit {
 
-    constructor() {}
+    id: string;
+    artist: Artist[];
+    album: Album[];
+    constructor(private _spotifyService: SpotifyService, private _route: ActivatedRoute) {
+        
+    }
+
+    ngOnInit() {
+        this._route.params
+        .map(params => {
+            return params['id'];
+        })
+        .subscribe(id => {
+            this._spotifyService.getArtist(id)
+                .subscribe(artist => {
+                    this.artist = artist;
+                })
+            
+        });
+    }
 }
